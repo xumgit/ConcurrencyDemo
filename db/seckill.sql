85ac9bc5fa7d8bd86b43254037392dc27dbcded2
xum <xum1281517532@163.com>
2020-05-24 11:55:25 +0800

    add sql file
    add seckill database

---
+++ db/seckill.sql
@@ -0,0 +1,80 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : dataBase
+ Source Server Type    : MySQL
+ Source Server Version : 50528
+ Source Host           : localhost:3306
+ Source Schema         : seckill
+
+ Target Server Type    : MySQL
+ Target Server Version : 50528
+ File Encoding         : 65001
+
+ Date: 24/05/2020 11:52:51
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for red_packet_record
+-- ----------------------------
+DROP TABLE IF EXISTS `red_packet_record`;
+CREATE TABLE `red_packet_record`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `red_packet_id` bigint(20) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for red_racket
+-- ----------------------------
+DROP TABLE IF EXISTS `red_racket`;
+CREATE TABLE `red_racket`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `red_packet_id` bigint(20) NOT NULL,
+  `total_amount` int(11) NOT NULL,
+  `total_packet` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for seckill
+-- ----------------------------
+DROP TABLE IF EXISTS `seckill`;
+CREATE TABLE `seckill`  (
+  `seckill_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
+  `name` varchar(120) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '商品名称',
+  `number` int(11) NOT NULL COMMENT '库存数量',
+  `start_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '秒杀开启时间',
+  `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '秒杀结束时间',
+  `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  `version` int(11) NOT NULL COMMENT '版本号',
+  PRIMARY KEY (`seckill_id`) USING BTREE,
+  INDEX `idx_start_time`(`start_time`) USING BTREE,
+  INDEX `idx_end_time`(`end_time`) USING BTREE,
+  INDEX `idx_create_time`(`create_time`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1004 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '秒杀库存表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for success_killed
+-- ----------------------------
+DROP TABLE IF EXISTS `success_killed`;
+CREATE TABLE `success_killed`  (
+  `seckill_id` bigint(20) NOT NULL COMMENT '秒杀商品id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户Id',
+  `state` tinyint(4) NOT NULL COMMENT '状态标示：-1指无效，0指成功，1指已付款',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`seckill_id`, `user_id`) USING BTREE,
+  INDEX `idx_create_time`(`create_time`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '秒杀成功明细表' ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
